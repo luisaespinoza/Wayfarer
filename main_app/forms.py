a7e1ccc33229f73314e1691f844c94b3e6ec9304
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-03-16 12:21:01 -0700

    sign up form has custom fields now to reflect required fields.

--- main_app/forms.py
+++ main_app/forms.py
@@ -2,9 +2,16 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import User
 
-
-class UserCreationForm(UserCreationForm):
-  class Meta:
-    model = User
-    fields = ['username','email','city']
+class SignUpForm(UserCreationForm):
+    email=forms.EmailField(max_length=64)
+    username = forms.CharField()
+    first_name = forms.CharField(max_length=32)
+    last_name=forms.CharField(max_length=32)
+    city = forms.CharField(max_length=64)
+    password1=forms.CharField()
+    password2=forms.CharField()
+    
+    class Meta(UserCreationForm.Meta):
+      model = User
+      fields = UserCreationForm.Meta.fields + ('email','first_name','last_name','city')
 
