4b3dd445c00f9bce25307b0b1da6ae0d6533de38
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-03-15 19:17:35 -0700

    made progs toward auth. TODO: fix migration errors for CUSTOM USER

--- wayfarer_project/settings.py
+++ wayfarer_project/settings.py
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = "wayfarer_project.User"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# LOGIN_REDIRECT_URL = '/profile/'
