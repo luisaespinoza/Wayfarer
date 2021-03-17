342a41b9694942a21c0bd59028ee815160905f4d
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-03-17 16:09:19 -0700

    Edit Profile Functionality Added

--- main_app/forms.py
+++ main_app/forms.py
@@ -13,25 +13,7 @@ class SignUpForm(UserCreationForm):
       model = User
       fields = UserCreationForm.Meta.fields + ('email','first_name','last_name','city')
 
-<<<<<<< HEAD
-class EditForm(UserChangeForm):
-  class Meta:
-    model = User
-    fields = 
-  # new fields here
-
-  class Meta(UserChangeForm):
-    model = User
-    fields = UserChangeForm.Meta.fields + ('new fields here')
-=======
-# class EditForm(UserCreationForm):
-#     email=forms.EmailField(max_length=64)
-#     username = forms.CharField()
-#     first_name = forms.CharField(max_length=32)
-#     last_name=forms.CharField(max_length=32)
-#     city = forms.CharField(max_length=64)
-
-#     class Meta(UserCreationForm.Meta):
-#       model = User
-#       fields = UserCreationForm.Meta.fields + ('email','first_name','last_name','city')
->>>>>>> submain
+class EditUserForm(forms.ModelForm):
+    class Meta(UserChangeForm.Meta):
+      model = User
+      fields = ('email','username','first_name','last_name','city')
