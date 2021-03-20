1a00cf5017e8a407c120d2b6ec4f2ee742764027
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-03-20 14:12:44 -0700

    file structure changes.

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
   # path('admin/', a)
   path('', views.home, name='home'),
-  path('post/new', views.new_post, name='new_post'),
-  path('post/<int:post_id>/edit', views.edit_post, name='edit_post'),
-  path('post/<int:post_id>/details/', views.post_details, name='post_details'),
-  path('post/<int:post_id>/delete' , views.delete_post, name='delete_post'),
+  path('posts/new', views.new_post, name='new_post'),
+  path('posts/<int:post_id>/edit', views.edit_post, name='edit_post'),
+  path('posts/<int:post_id>/details/', views.post_details, name='post_details'),
+  path('posts/<int:post_id>/delete' , views.delete_post, name='delete_post'),
   # path('user_login/', views.user_login, name='user_login'),
   path('accounts/', include('django.contrib.auth.urls')),
   path('cities/', views.cities, name='cities'),
@@ -16,5 +16,4 @@ urlpatterns = [
   path('accounts/signup', views.signup, name='signup'),
   path('accounts/profile/', views.profile, name='profile'),
   path('accounts/profile_edit/', views.profile_edit, name='profile_edit'),
-
 ]
