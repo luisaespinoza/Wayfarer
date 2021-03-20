229f6cf45f9ed1f5fb8078972d0daef74f7b4993
LUIS ESPINOZA <coco@LUISs-MacBook-Pro.local>
2021-03-20 14:35:30 -0700

    housekeeping

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,13 +3,11 @@ from . import views
 
 
 urlpatterns = [
-  # path('admin/', a)
   path('', views.home, name='home'),
   path('posts/new', views.new_post, name='new_post'),
   path('posts/<int:post_id>/edit', views.edit_post, name='edit_post'),
   path('posts/<int:post_id>/details/', views.post_details, name='post_details'),
   path('posts/<int:post_id>/delete' , views.delete_post, name='delete_post'),
-  # path('user_login/', views.user_login, name='user_login'),
   path('accounts/', include('django.contrib.auth.urls')),
   path('cities/', views.cities, name='cities'),
   path('cities/<int:city_id>', views.cities_details, name='cities_details'),
